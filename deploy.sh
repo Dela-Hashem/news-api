1f6d3a3dd961ed4d35abf09fa08d367dfc8fc93f
Dela Hashem <Dela.Hashem89@gmail.com>
2025-06-11 00:46:04 +0800

    Fix: Add deploy.sh with disown and exit 0

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 pkill -f 'newsapi' || echo "Nothing to kill"
-nohup java -jar /home/ec2-user/news-api/target/*.jar > app.log 2>&1 < /dev/null & disown
+nohup java -jar /home/ec2-user/news-api/target/*.jar > app.log 2>&1 < /dev/null &
+disown
+exit 0
