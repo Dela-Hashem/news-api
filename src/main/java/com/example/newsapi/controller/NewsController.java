7ad8eea78494f4f55be4acd14abecea27605df50
Dela Hashem <Dela.Hashem89@gmail.com>
2025-06-11 13:24:30 +0800

    add a info log for test

--- src/main/java/com/example/newsapi/controller/NewsController.java
+++ src/main/java/com/example/newsapi/controller/NewsController.java
@@ -2,6 +2,8 @@ package com.example.newsapi.controller;
 
 import com.example.newsapi.model.News;
 import com.example.newsapi.service.NewsService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +15,7 @@ import java.util.Optional;
 public class NewsController {
 
     private final NewsService newsService;
+    private static final Logger log = LoggerFactory.getLogger(NewsController.class);
 
     public NewsController(NewsService newsService) {
         this.newsService = newsService;
@@ -20,6 +23,7 @@ public class NewsController {
 
     @GetMapping
     public List<News> getAllNews() {
+        log.info("✅ News endpoint was hit");
         return newsService.getAllNews();
     }
 
