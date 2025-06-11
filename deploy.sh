dc4af266f4fbbaff16df99adc721d9a87dcc410a
Dela Hashem <Dela.Hashem89@gmail.com>
2025-06-11 14:13:51 +0800

    Fix: Ensure deploy.sh deletes old JAR before starting new one

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 pkill -f 'newsapi' || echo "Nothing to kill"
+rm -f /home/ec2-user/news-api/target/*.jar.original
+rm -f /home/ec2-user/news-api/target/*.jar
 
 nohup java -jar /home/ec2-user/news-api/target/*.jar > app.log 2>&1 < /dev/null &
 
