94d89b7be1f14a220d894f1f92689da59364d87b
Dela Hashem <Dela.Hashem89@gmail.com>
2025-06-11 15:29:57 +0800

    Fix jar file path for EC2 copy

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 pkill -f 'newsapi' || echo "Nothing to kill"
-rm -f /home/ec2-user/news-api/target/*.jar.original
-rm -f /home/ec2-user/news-api/target/*.jar
 
-nohup java -jar /home/ec2-user/news-api/target/*.jar > app.log 2>&1 < /dev/null &
+sleep 2
 
-sleep 3
+nohup java -jar /home/ec2-user/news-api/target/*.jar > /home/ec2-user/news-api/app.log 2>&1 < /dev/null &
+
 disown
+sleep 3
 exit 0
