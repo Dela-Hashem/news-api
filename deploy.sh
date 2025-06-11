99731c812197741f11d7b156a3e469c613472ab3
Dela Hashem <Dela.Hashem89@gmail.com>
2025-06-11 16:09:46 +0800

    Fix: edit target and jar file address

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ pkill -f 'newsapi' || echo "Nothing to kill"
 
 rm -f /home/ec2-user/news-api/target/*.jar.original
 
-nohup java -jar /home/ec2-user/news-api/target/newsapi-0.0.1-SNAPSHOT.jar > /home/ec2-user/news-api/app.log 2>&1 < /dev/null &
+nohup java -jar /home/ec2-user/news-api/newsapi-0.0.1-SNAPSHOT.jar > /home/ec2-user/news-api/app.log 2>&1 < /dev/null &
 
 sleep 3
 disown
